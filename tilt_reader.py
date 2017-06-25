0e94a07e1e884b195a49ad3930ff8311649fff6e
LaustChristensen <laust.christensen@gmail.com>
2017-06-25 14:05:35 +0200

    TiltReader
    TiltReader kan nu opdatere på heartbeat

--- tilt_reader.py
+++ tilt_reader.py
@@ -1,22 +1,39 @@
 from tkinter import *
 import tkinter.font
 import time
-from datetime import date
+from time import strftime, gmtime
+
+## variables
+updateSecs = 600 #time in seconds between updating the google sheet
+screenSecs = 3000 #time in miliseconds until blanking the display
 
 
-##GUi definitions
+##GUI definitions
 win = Tk()
 win.title("Tilt reader")
 myFont = tkinter.font.Font(family = 'Helvetiva', size = 12, weight = "bold")
 
-def readToggle():
-    theTime.set(time.localtime())
+def updateTime():
+    theTime.set(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
 
-## variables
+def updateLoop():
+    updateTime()
+    win.after(screenSecs, updateLoop)
+            
+
+## variable variables
 theTime = StringVar()
 
 ## Widgets
-myText = Label(win, textvariable = theTime)
-myText.pack()
-myButton = Button(win, text = 'Refresh', font = myFont, command = readToggle, bg = 'bisque2', height = 1, width = 24)
-myButton.pack()
+
+def main():
+    myText = Label(win, textvariable = theTime)
+    myText.pack()
+    myButton = Button(win, text = 'Refresh', font = myFont, command = updateTime, bg = 'bisque2', height = 1, width = 24)
+    myButton.pack()
+    screenTime = time.time() + screenSecs
+    updateTime()
+    win.after(screenSecs, updateLoop)
+    win.mainloop()
+
+main()
