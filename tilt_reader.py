93d80df34e87d5c53c64a4da788a9ea73b0f12c1
LaustChristensen <laust.christensen@gmail.com>
2017-06-25 21:42:11 +0200

    Ekstra data
    Ekstra felter er tilføjet. Program bør skrives om til af være mere i
    tråd med funktionel programmering (ingen globale variable)

--- tilt_reader.py
+++ tilt_reader.py
@@ -1,38 +1,71 @@
 from tkinter import *
 import tkinter.font
+import datetime
 import time
 from time import strftime, gmtime
+import requests
 
 ## variables
 updateSecs = 600 #time in seconds between updating the google sheet
 screenSecs = 3000 #time in miliseconds until blanking the display
-
+initialSG = float(1050)/1000
 
 ##GUI definitions
 win = Tk()
 win.title("Tilt reader")
 myFont = tkinter.font.Font(family = 'Helvetiva', size = 12, weight = "bold")
 
-def updateTime():
+## Functions
+
+#function to calculate the number of days since epoch (used by google sheets)
+#In python time.time() gives number of seconds since epoch (Jan 1 1970).
+#Google Sheets datetime as a number is the number of days since the epoch except their epoch date is Jan 1 1900
+def sheetsDate(date1):
+	temp = datetime.datetime(1899, 12, 30)
+	delta=date1-temp
+	return float(delta.days) + (float(delta.seconds) / 86400)
+
+def updateData():
     theTime.set(strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()))
+    theTemp.set("25")
+    theSG.set(initialSG)
+    theColour.set("Black")
+    theBeer.set("Test beer")
 
 def updateLoop():
-    updateTime()
+    updateData()
     win.after(screenSecs, updateLoop)
             
+def calcSG(sg):
+        return sg-float(1/1000)
+
 
 ## variable variables
 theTime = StringVar()
+theTemp = StringVar()
+theSG = StringVar()
+theColour = StringVar()
+theBeer = StringVar()
 
 ## Widgets
 
 def main():
-    myText = Label(win, textvariable = theTime)
-    myText.pack()
-    myButton = Button(win, text = 'Refresh', font = myFont, command = updateTime, bg = 'bisque2', height = 1, width = 24)
+    myTime = Label(win, textvariable = theTime)
+    myTime.pack()
+    myTemp = Label(win, textvariable = theTemp)
+    myTemp.pack()
+    mySG = Label(win, textvariable = theSG)
+    mySG.pack()
+    myColour = Label(win, textvariable = theColour)
+    myColour.pack()
+    myBeer = Label(win, textvariable = theBeer)
+    
+    myButton = Button(win, text = 'Refresh', font = myFont, command = updateData, bg = 'bisque2', height = 1, width = 24)
     myButton.pack()
+
     screenTime = time.time() + screenSecs
-    updateTime()
+    updateData()
+
     win.after(screenSecs, updateLoop)
     win.mainloop()
 
